0e16ab71238ffc28fe4693e74c51f2f9b33a6d5a
lobsterdev0 <lobsterdev0@gmail.com>
2021-09-28 15:03:16 +0300

    update timestamp

--- app/config.ts
+++ app/config.ts
@@ -1,6 +1,6 @@
 module.exports = {
     startTimestamp: 1632587457,
-    endTimestamp: 1632587457 + 84 * 60 * 60,
+    endTimestamp: 1632587457 + 84 * 60 * 60 + 48 * 60 * 60,
     admins: (process.env.ADMIN_IDS || '').split(',').map(i => parseInt(i)),
     remainedMultisig: '0x557068A9b7d66F97A61b97C80541eb17672E1e6f',
     totalTokensCount: 6501
